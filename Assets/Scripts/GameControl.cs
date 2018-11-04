cf581f22e3f8cac852f05bc01141936725cb7621
Ian Jones <ianjones@stanford.edu>
2018-11-04 14:08:14 -0800

    before trying to make different backgrounds

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+
+    private int score = 0;
+
+	void Awake () 
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+
+	void Update () 
+    {
+        if (gameOver && Input.GetMouseButton (0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+		
+	}
+
+
+    public void Died()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
---
+++ Assets/Scripts/SnowballMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballMovement : MonoBehaviour {
+
+    public float upForce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+
+	void Start () 
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	    
+	}
+	
+	void Update () 
+    {
+        // Check if rolling backwards, if so you lose
+        //if (rb2d.velocity.x < 0) isDead = true;
+
+
+        if (!isDead)
+        {
+            // Click to jump
+            // Need to add physics here?
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        } else 
+        {
+            endGame();
+        }
+	}
+
+    void endGame() 
+    {
+        Debug.Log("GAME OVER");
+        GameControl.instance.Died();
+    }
+}

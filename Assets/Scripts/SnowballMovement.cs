a870cb1c41814d4762cf7d02db7b502a83a50e17
Austin Jones <austinhj@stanford.edu>
2018-11-04 18:23:56 -0800

    Snowball grows/shrinks when on/off ground.

--- Assets/Scripts/SnowballMovement.cs
+++ Assets/Scripts/SnowballMovement.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class SnowballMovement : MonoBehaviour {
 
     public float upForce = 200f;
+    public float growthRate = 0.01f;
+    public float maxSize = 5f;
+    public float minSize = 0.5f;
+    public float threshholdSpeed = 5f;
     public bool isGrounded;
     public LayerMask groundLayers;
 
@@ -22,15 +26,26 @@ public class SnowballMovement : MonoBehaviour {
 
     /* Checks:
        1) Only allows jumping when player !isDead and isGrounded.
-       2) Size of snowball increases when isGrounded, decreases when !isGrounded.
+       2) Size of snowball increases when isGrounded, decreases when !isGrounded, with min & max sizes.
     */
     void Update()
     {
         float radius = snowballCollider.radius * transform.localScale[0] + 0.1f;
         isGrounded = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y), radius, groundLayers);
+
+        if (isGrounded && transform.localScale[0] < maxSize) {
+            // Grow if maxSize hasn't been reached
+            transform.localScale += new Vector3(growthRate, growthRate, 0);
+        } else if (!isGrounded && transform.localScale[0] > minSize && rb2d.velocity[1] < threshholdSpeed)
+        {
+            // Shrink if minSize hasn't been reached
+            transform.localScale -= new Vector3(growthRate, growthRate, 0);
+        }
+
+
+
         // Check if rolling backwards, if so you lose
         //if (rb2d.velocity.x < 0) isDead = true
-
         if (!isDead)
         {
             /* Need to add physics here */

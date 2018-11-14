507aea65c0dff974a2d281f1ade6069ce4e1f7dd
Ian Jones <ianjones@stanford.edu>
2018-11-14 15:59:37 -0800

    reverted to locking player position, implemented normal vector stuff...

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidbody;
+    //private Animator myAnimator;
+
+    // Movement stuff
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float jumpForce;
+
+    // Grounding stuff
+    [SerializeField] private Transform[] groundPoints;
+    [SerializeField] private float groundRadius;
+    [SerializeField] LayerMask whatIsGround;
+    private bool isGrounded;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        //myAnimator = GetComponent<Animator>();
+    }
+
+
+    void FixedUpdate()
+    {
+        isGrounded = IsGrounded();
+
+        //if (isGrounded) 
+        //{
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            GameControl.instance.norm = hit.normal;
+        //}
+    }
+
+
+    private bool IsGrounded()
+    {
+        foreach (Transform point in groundPoints)
+        {
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position,
+                                                                groundRadius,
+                                                                whatIsGround);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i].gameObject != gameObject)
+                {
+                    //myAnimator.SetBool("isJumping", false);
+                    //myAnimator.SetBool("isLanding", false);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
--- Assets/Scripts/Scrolling scripts/ScrollingForeground.cs
+++ Assets/Scripts/Scrolling scripts/ScrollingForeground.cs
@@ -14,7 +14,18 @@ public class ScrollingForeground: MonoBehaviour {
 
 	void Update() 
     {
-        rb2d.velocity = new Vector2(GameControl.instance.foregroundXScrollSpeed, GameControl.instance.foregroundYScrollSpeed);
+        /* Compute perpindicular vector to the normal (basically compute slope of terrain) */
+        Vector2 perp = new Vector2(-GameControl.instance.norm.y, GameControl.instance.norm.x);
+        Vector2 midAir = new Vector2(0f, 0f); // Player is midair, just scroll x direction
+
+        float xVel = GameControl.instance.norm != midAir ? perp.normalized.x : -1.0f;
+        float yVel = GameControl.instance.norm != midAir ? perp.normalized.y : 0f;
+
+        /* Scroll speed computed from the normalized vector */
+        rb2d.velocity = new Vector2(xVel * GameControl.instance.scrollSpeed, yVel * GameControl.instance.scrollSpeed);
+        //rb2d.velocity = new Vector2(GameControl.instance.foregroundXScrollSpeed, GameControl.instance.foregroundYScrollSpeed);
+
+        /* Stop scrolling on game over */
         if (GameControl.instance.gameOver) rb2d.velocity = Vector2.zero;
     }
 }
